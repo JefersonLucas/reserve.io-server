c9f572d437353b87db60d1cc3346628c6969e20b
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-10 16:53:26 -0300

    Database connection created

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import app from './app'
+import mongoose from 'mongoose'
+import 'dotenv/config'
 
-app.listen(3333)
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.DB_MONGODB_USER}:${process.env.DB_MONGODB_PASSWORD}@apicluster.v91ac.mongodb.net/${process.env.DB_MONGODB_DATABASE}?retryWrites=true&w=majority`
+	)
+	.then(() => {
+		console.log('Database connection created successfully!')
+		console.log(`Open port on http://localhost:${process.env.PORT}/`)
+		app.listen(process.env.PORT)
+	})
+	.catch((e) => {
+		console.log(e.message)
+	})
