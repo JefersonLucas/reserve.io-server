c694ca174abe3f4eb5cce23016e8b8f2241f294a
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-11 13:51:36 -0300

    Created getUser controller

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -7,17 +7,22 @@ async function createUser(req: Request, res: Response): Promise<void> {
 	try {
 		const { username, email, password } = req.body
 
-		if (!username) throw new Error('username field is required')
-
-		if (!email) throw new Error('email field is required')
-
-		if (!password) throw new Error('password field is required')
-
-		const user: User = {
-			username,
-			email,
-			password
+		if (!username) {
+			res.status(422).json({ error: 'campo nome de usuário é obrigatório.' })
+			return
 		}
+
+		if (!email) {
+			res.status(422).json({ error: 'campo de e-mail é obrigatório.' })
+			return
+		}
+
+		if (!password) {
+			res.status(422).json({ error: 'campo de senha é obrigatório.' })
+			return
+		}
+
+		const user: User = { username, email, password }
 
 		await UserService.createUser(user)
 
@@ -31,7 +36,40 @@ async function getUsers(_: Request, res: Response): Promise<void> {
 	try {
 		const users = await UserService.getUsers()
 
-		res.status(201).json(users)
+		res.status(201).json(
+			users.map(({ _id, username, email, password }) => {
+				return {
+					id: _id,
+					username,
+					email,
+					password
+				}
+			})
+		)
+	} catch (error) {
+		res.status(500).json({ error: error.message })
+	}
+}
+
+async function getUser(req: Request, res: Response): Promise<void> {
+	try {
+		const { id } = req.params
+
+		const user = await UserService.getUser(id)
+
+		if (!user) {
+			res.status(422).json({ error: 'id não encontrado.' })
+			return
+		}
+
+		const { _id, username, email, password } = user
+
+		res.status(201).json({
+			id: _id,
+			username,
+			email,
+			password
+		})
 	} catch (error) {
 		res.status(500).json({ error: error.message })
 	}
@@ -39,5 +77,6 @@ async function getUsers(_: Request, res: Response): Promise<void> {
 
 export default {
 	createUser,
-	getUsers
+	getUsers,
+	getUser
 }
