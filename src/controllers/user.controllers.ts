b5ddc488ed83a224a2312b0f3527a5a83d2d567a
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-11 20:06:07 -0300

    Modified create User function new validations and created login function with jwt

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,32 +1,116 @@
 import { Request, Response } from 'express'
+import { compare, genSalt, hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import 'dotenv/config'
 
 import { User } from '@types'
+
 import UserService from '@services/user.services'
+import UserModel from '@models/User'
+
+const secret = process.env.SECRET_JWT
 
 async function createUser(req: Request, res: Response): Promise<User> {
 	try {
-		const { username, email, password } = req.body
+		const { username, email, password, confirm_password } = req.body
 
 		if (!username) {
-			res.status(422).json({ error: 'campo nome de usuário é obrigatório.' })
+			res.status(422).json({ error: 'o campo nome de usuário é obrigatório.' })
 			return
 		}
 
 		if (!email) {
-			res.status(422).json({ error: 'campo de e-mail é obrigatório.' })
+			res.status(422).json({ error: 'o campo de e-mail é obrigatório.' })
 			return
 		}
 
 		if (!password) {
-			res.status(422).json({ error: 'campo de senha é obrigatório.' })
+			res.status(422).json({ error: 'o campo de senha é obrigatório.' })
 			return
 		}
 
-		const user: User = { username, email, password }
+		if (!confirm_password) {
+			res
+				.status(422)
+				.json({ error: 'o campo confirmação de senha é obrigatório.' })
+			return
+		}
+
+		if (password !== confirm_password) {
+			res.status(422).json({ error: 'as senhas não conferem.' })
+			return
+		}
+
+		const userExists = await UserService.getUserEmail(email)
+
+		if (userExists) {
+			res.status(422).json({ error: 'esse e-mail já foi utilizado.' })
+			return
+		}
+
+		const salt = await genSalt(12)
+
+		const password_hash = await hash(password, salt)
+
+		const user = new UserModel({
+			username,
+			email,
+			password: password_hash
+		})
 
 		await UserService.createUser(user)
 
-		res.status(201).json(user)
+		res.status(201).json({
+			id: user._id,
+			username: user.username,
+			password: user.password
+		})
+	} catch (error) {
+		res.status(500).json({ error: error.message })
+	}
+}
+
+async function login(req: Request, res: Response) {
+	try {
+		const { email, password } = req.body
+
+		if (!email) {
+			res.status(422).json({ error: 'o campo de e-mail é obrigatório.' })
+			return
+		}
+
+		if (!password) {
+			res.status(422).json({ error: 'o campo de senha é obrigatório.' })
+			return
+		}
+
+		const user = await UserService.getUserEmail(email)
+
+		if (!user) {
+			res.status(404).json({ error: 'usuário não encontrado.' })
+			return
+		}
+
+		const check_password = await compare(password, user.password)
+
+		if (!check_password) {
+			res.status(422).json({ error: 'senha inválida.' })
+			return
+		}
+
+		const token = sign(
+			{
+				id: user._id
+			},
+			secret
+		)
+
+		res.status(200).json({
+			id: user._id,
+			username: user.username,
+			password: user.password,
+			token: token
+		})
 	} catch (error) {
 		res.status(500).json({ error: error.message })
 	}
@@ -36,13 +120,12 @@ async function getUsers(_: Request, res: Response): Promise<User[] | void> {
 	try {
 		const users = await UserService.getUsers()
 
-		res.status(201).json(
-			users.map(({ _id, username, email, password }) => {
+		res.status(200).json(
+			users.map(({ _id, username, email }) => {
 				return {
 					id: _id,
 					username,
-					email,
-					password
+					email
 				}
 			})
 		)
@@ -62,13 +145,12 @@ async function getUser(req: Request, res: Response): Promise<User> {
 			return
 		}
 
-		const { _id, username, email, password } = user
+		const { _id, username, email } = user
 
-		res.status(201).json({
+		res.status(200).json({
 			id: _id,
 			username,
-			email,
-			password
+			email
 		})
 	} catch (error) {
 		res.status(500).json({ error: error.message })
@@ -82,17 +164,17 @@ async function updateUser(req: Request, res: Response): Promise<User> {
 		const { id } = req.params
 
 		if (!username) {
-			res.status(422).json({ error: 'campo nome de usuário é obrigatório.' })
+			res.status(422).json({ error: 'o campo nome de usuário é obrigatório.' })
 			return
 		}
 
 		if (!email) {
-			res.status(422).json({ error: 'campo de e-mail é obrigatório.' })
+			res.status(422).json({ error: 'o campo de e-mail é obrigatório.' })
 			return
 		}
 
 		if (!password) {
-			res.status(422).json({ error: 'campo de senha é obrigatório.' })
+			res.status(422).json({ error: 'o campo de senha é obrigatório.' })
 			return
 		}
 
@@ -135,6 +217,7 @@ async function deleteUser(req: Request, res: Response): Promise<void> {
 
 export default {
 	createUser,
+	login,
 	getUsers,
 	getUser,
 	updateUser,
