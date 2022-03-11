da58cd539f4143807162f67db9c4e0c8d6db8590
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-11 09:28:41 -0300

    Created router getUsers

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,5 +5,6 @@ import UserController from '@controllers/user.controllers'
 const routes = express.Router()
 
 routes.post('/create', UserController.createUser)
+routes.get('/', UserController.getUsers)
 
 export default routes
