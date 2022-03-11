389937a42385a0bb5b359c2ee35c8a07de516895
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-11 14:54:33 -0300

    Created updateUser controller

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express'
 import { User } from '@types'
 import UserService from '@services/user.services'
 
-async function createUser(req: Request, res: Response): Promise<void> {
+async function createUser(req: Request, res: Response): Promise<User> {
 	try {
 		const { username, email, password } = req.body
 
@@ -32,7 +32,7 @@ async function createUser(req: Request, res: Response): Promise<void> {
 	}
 }
 
-async function getUsers(_: Request, res: Response): Promise<void> {
+async function getUsers(_: Request, res: Response): Promise<User[] | void> {
 	try {
 		const users = await UserService.getUsers()
 
@@ -51,7 +51,7 @@ async function getUsers(_: Request, res: Response): Promise<void> {
 	}
 }
 
-async function getUser(req: Request, res: Response): Promise<void> {
+async function getUser(req: Request, res: Response): Promise<User> {
 	try {
 		const { id } = req.params
 
@@ -75,8 +75,48 @@ async function getUser(req: Request, res: Response): Promise<void> {
 	}
 }
 
+async function updateUser(req: Request, res: Response): Promise<User> {
+	try {
+		const { username, email, password } = req.body
+
+		const { id } = req.params
+
+		if (!username) {
+			res.status(422).json({ error: 'campo nome de usuário é obrigatório.' })
+			return
+		}
+
+		if (!email) {
+			res.status(422).json({ error: 'campo de e-mail é obrigatório.' })
+			return
+		}
+
+		if (!password) {
+			res.status(422).json({ error: 'campo de senha é obrigatório.' })
+			return
+		}
+
+		const user: User = { username, email, password }
+
+		const updatedUser = await UserService.updateUser(id, user)
+
+		if (updatedUser.matchedCount === 0) {
+			res.status(422).json({ error: 'usuário não encontrado.' })
+			return
+		}
+
+		res.status(201).json({
+			id,
+			...user
+		})
+	} catch (error) {
+		res.status(500).json({ error: error.message })
+	}
+}
+
 export default {
 	createUser,
 	getUsers,
-	getUser
+	getUser,
+	updateUser
 }
