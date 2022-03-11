9d940f222e81efc2a1b6bda1d287ee0d33e3ea32
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-11 09:28:13 -0300

    Created services getUsers

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -6,6 +6,11 @@ async function createUser(user: User): Promise<User> {
 	return await UserRepository.createUser(user)
 }
 
+async function getUsers(): Promise<User[]> {
+	return await UserRepository.getUsers()
+}
+
 export default {
-	createUser
+	createUser,
+	getUsers
 }
