84ecc6f92dbcdbe4bc3ac74eb80c392bc7249902
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-17 17:38:12 -0300

    Created get reservation router

--- src/routes/reservation.routes.ts
+++ src/routes/reservation.routes.ts
@@ -6,5 +6,6 @@ import authorization from '@middlewares/authorization'
 const routes = express.Router()
 
 routes.post('/register', authorization, ReservationController.createReservation)
+routes.get('/', authorization, ReservationController.getReservations)
 
 export default routes
