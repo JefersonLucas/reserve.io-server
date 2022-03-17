cf4529194d2c56921fdde3eb8870e71f3d26f26c
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-17 11:02:38 -0300

    Created Status type and Reservation interface

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,21 @@
+/* eslint-disable no-tabs */
+/* eslint-disable camelcase */
 import { Types } from 'mongoose'
+
+export type Status = 'waiting' | 'using' | 'collected'
 export interface User {
 	_id?: Types.ObjectId
 	username: string
 	email: string
 	password: string
+}
+
+export interface Reservation {
+	_id?: Types.ObjectId
+	status: Status
+	requester: string
+	date: Date
+	place: string
+	entry_time: string
+	exit_time: string
 }
