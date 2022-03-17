feb39ce8ec911a4c3e9a07e41d2a02dc880a3499
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-03-17 17:37:28 -0300

    Created get reservation service

--- src/services/reservation.services.ts
+++ src/services/reservation.services.ts
@@ -1,10 +1,15 @@
-import ReservationRepositories from '@repositories/reservation.repositories'
+import ReservationRepository from '@repositories/reservation.repositories'
 import { Reservation } from '@types'
 
 async function createReservation (reservation: Reservation) {
-  return await ReservationRepositories.createReservation(reservation)
+  return await ReservationRepository.createReservation(reservation)
+}
+
+async function getReservations (): Promise<Reservation[]> {
+  return await ReservationRepository.getReservations()
 }
 
 export default {
-  createReservation
+  createReservation,
+  getReservations
 }
