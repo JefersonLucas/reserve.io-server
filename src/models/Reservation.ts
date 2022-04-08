e4b72bb658665a118d4602622e8922dfb4352086
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-04-07 21:15:58 -0300

    Updated model Reservation with interface

--- src/models/Reservation.ts
+++ src/models/Reservation.ts
@@ -1,15 +1,17 @@
 import { Schema, model } from 'mongoose'
 import { Reservation } from '@types'
 
-const reservationSchema = new Schema<Reservation>({
-  status: String,
+const ReservationSchema = new Schema<Reservation>({
   requester: String,
+  equipment: String,
+  status: String,
   date: String,
   place: String,
   entry_time: String,
-  exit_time: String
+  exit_time: String,
+  observation: String
 })
 
-const ReservationModel = model<Reservation>('Reservation', reservationSchema)
+const ReservationModel = model<Reservation>('Reservation', ReservationSchema)
 
 export default ReservationModel
