94c0e237c1177d78a6fccb9a28d395efd178adb1
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-04-07 21:58:41 -0300

    Updated post and delete routes

--- src/routes/reservation.routes.ts
+++ src/routes/reservation.routes.ts
@@ -5,10 +5,10 @@ import authorization from '@middlewares/authorization'
 
 const routes = express.Router()
 
-routes.post('/register', authorization, ReservationController.createReservation)
+routes.post('/create', authorization, ReservationController.createReservation)
 routes.get('/', authorization, ReservationController.getReservations)
 routes.get('/:id', authorization, ReservationController.getReservation)
 routes.put('/update/:id', authorization, ReservationController.updateReservation)
-routes.delete('/:id', authorization, ReservationController.deleteReservation)
+routes.delete('/delete/:id', authorization, ReservationController.deleteReservation)
 
 export default routes
