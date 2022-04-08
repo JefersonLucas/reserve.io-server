b58311b7bbc28a648fc360ee3a9abae96778922a
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-04-07 21:55:11 -0300

    Creating service to update reservation

--- src/services/reservation.services.ts
+++ src/services/reservation.services.ts
@@ -1,5 +1,6 @@
 import { Reservation } from '@types'
 import { DeleteResult } from 'mongodb'
+import { UpdateWriteOpResult } from 'mongoose'
 import ReservationRepository from '@repositories/reservation.repositories'
 
 async function createReservation (reservation: Reservation) {
@@ -14,6 +15,10 @@ async function getReservation (id: string): Promise<Reservation> {
   return await ReservationRepository.getReservation(id)
 }
 
+async function updateReservation (id: string, reservation: Reservation): Promise<UpdateWriteOpResult> {
+  return await ReservationRepository.updateReservation(id, reservation)
+}
+
 async function deleteReservation (id: string): Promise<DeleteResult> {
   return await ReservationRepository.deleteReservation(id)
 }
@@ -22,5 +27,6 @@ export default {
   createReservation,
   getReservations,
   getReservation,
+  updateReservation,
   deleteReservation
 }
